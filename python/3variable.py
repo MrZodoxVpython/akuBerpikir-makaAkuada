cff4d9e789b6c78019c39820b8cc3d1380ea44f3
benjamin.wickman <wickman@MrZodoxVpython.hackerone.com>
2024-09-19 10:07:25 -0400

    DAY3

---
+++ python/3variable.py
@@ -0,0 +1,19 @@
+#Variable = A container for a value (String, integer, float,boolean) 
+# A variable behaves as if it was the value it contains
+
+
+#String
+first_name = "Dude"
+food = "pizza"
+email = "dudepizza404@hacker.com"
+
+#Integer
+age = 23
+id_student = 5
+num_student = 23
+
+#Float
+price = 10.99
+gps = 3.2
+
+print(f"The price is ${price}")
