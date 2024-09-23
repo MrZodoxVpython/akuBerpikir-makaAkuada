822e7d5d6912d77124ccdc5755ed73ea4ae6401e
Benjamin.Wickman <wickman@MrZodoxVpython.hackerone.com>
2024-09-23 10:44:54 -0400

    add sigma to repo

---
+++ programming/python/sigma.py
@@ -0,0 +1,14 @@
+def kelas():
+    print("I plot 1")
+    print("hi nice to meet you")
+
+kelas()
+kelas()
+kelas()
+
+def nama():
+    print("my name davin")
+def umur():
+    nama()
+    print("i'm 29th")
+umur()
